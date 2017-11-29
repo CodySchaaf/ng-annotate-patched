79b1d89c2212f8195f3ca23a335590683cf29be3
CodySchaaf <schaaf@sigfig.com>
2017-11-29 13:45:09 -0800

    Adds support for dynamic imports

--- src/traverse.js
+++ src/traverse.js
@@ -1,16 +1,20 @@
 // traverse.js
 // MIT licensed, see LICENSE file
 
-const walk = require("acorn/dist/walk");
+const defaultWalk = require("acorn/dist/walk");
+const dynamicImportWalk = require('acorn-dynamic-import/lib/walk').inject(defaultWalk);
 
-module.exports = function traverse(rootNode, options) {
+module.exports = function traverse(rootNode, options, pluginOptions = {}) {
+    const walk = pluginOptions.dynamicImport ? dynamicImportWalk : defaultWalk;
     const ancestors = [];
     (function c(node, st, override) {
         const parent = ancestors[ancestors.length - 1];
         const isNew = node !== parent;
         if (options.pre && isNew) options.pre(node, parent);
         if (isNew) ancestors.push(node);
-        walk.base[override || node.type](node, st, c);
+        let fn = override || node.type;
+        // Since Import isn't part of default acorn, using closest match
+        walk.base[fn](node, st, c);
         if (isNew) ancestors.pop();
         if (options.post && isNew) options.post(node, parent);
     })(rootNode);
